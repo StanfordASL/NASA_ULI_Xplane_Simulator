1b3fcf77306d420740b60c4bf5ed0c30521b5c7e
csandeep90 <vss.emvr@gmail.com>
2021-03-29 20:55:02 -0700

    alternate train

---
+++ src/train_DNN/alternate_train.py
@@ -0,0 +1,208 @@
+import time
+import copy
+import torch
+import numpy as np
+from tqdm.autonotebook import tqdm as tqdm
+from torch.utils.tensorboard import SummaryWriter
+
+from model_taxinet import TaxiNetDNN, freeze_model
+from taxinet_dataloader import *
+from plot_utils import *
+
+# make sure this is a system variable in your bashrc
+NASA_ULI_ROOT_DIR=os.environ['NASA_ULI_ROOT_DIR']
+
+DATA_DIR = NASA_ULI_ROOT_DIR + '/data/'
+
+# where intermediate results are saved
+# never save this to the main git repo
+SCRATCH_DIR = NASA_ULI_ROOT_DIR + '/scratch/'
+
+UTILS_DIR = NASA_ULI_ROOT_DIR + '/src/utils/'
+sys.path.append(UTILS_DIR)
+
+from textfile_utils import *
+
+
+def train_model(model, datasets, dataloaders, dist_fam, optimizer, device, num_epochs=25, log_every=100):
+    """
+    Trains a model on datatsets['train'] using criterion(model(inputs), labels) as the loss.
+    Returns the model with lowest loss on datasets['val']
+    Puts model and inputs on device.
+    Trains for num_epochs passes through both datasets.
+    
+    Writes tensorboard info to ./runs/ if given
+    """
+    writer = None
+    writer = SummaryWriter()
+        
+    model = model.to(device)
+    
+    since = time.time()
+
+    dataset_sizes = {x: len(datasets[x]) for x in ['train', 'val']}
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_loss = np.inf
+    
+    tr_loss = np.nan
+    val_loss = np.nan
+    
+    n_tr_batches_seen = 0
+    
+    with tqdm(total=num_epochs, position=0) as pbar:
+        pbar2 = tqdm(total=dataset_sizes['train'], position=1)
+        for epoch in range(num_epochs):
+            # Each epoch has a training and validation phase
+            for phase in ['train', 'val']:
+                if phase == 'train':
+                    model.train()  # Set model to training mode
+                else:
+                    model.eval()   # Set model to evaluate mode
+
+                running_loss = 0.0
+
+                running_tr_loss = 0.0 # used for logging
+
+                running_n = 0
+                
+                # Iterate over data.
+                pbar2.refresh()
+                pbar2.reset(total=dataset_sizes[phase])
+                for inputs, labels in dataloaders[phase]:
+                    if phase == 'train':
+                        n_tr_batches_seen += 1
+                    
+                    inputs = inputs.to(device)
+                    labels = labels.to(device)
+
+                    # zero the parameter gradients
+                    optimizer.zero_grad()
+
+                    # forward
+                    # track history if only in train
+                    with torch.set_grad_enabled(phase == 'train'):
+                        outputs = model(inputs)
+
+                        # loss
+                        ####################
+                        loss = loss_func(outputs, labels).mean()
+                        ####################
+                        
+                        # backward + optimize only if in training phase
+                        if phase == 'train':
+                            loss.backward()
+                            optimizer.step()
+
+                    # statistics
+                    running_loss += loss.item() * inputs.shape[0]
+                    
+                    if phase =='train':
+                        running_n += inputs.shape[0]
+                        running_tr_loss += loss.item() * inputs.shape[0]
+                        
+                        if n_tr_batches_seen % log_every == 0:
+                            mean_loss = running_tr_loss / running_n
+                            
+                            writer.add_scalar('loss/train', mean_loss, n_tr_batches_seen)
+                            
+                            running_tr_loss = 0.
+                            running_n = 0
+                    
+                    pbar2.set_postfix(split=phase, batch_loss=loss.item())
+                    pbar2.update(inputs.shape[0])
+                    
+
+                
+                epoch_loss = running_loss / dataset_sizes[phase]
+                
+                if phase == 'train':
+                    tr_loss = epoch_loss
+                    
+                if phase == 'val':
+                    val_loss = epoch_loss
+                    writer.add_scalar('loss/val', val_loss, n_tr_batches_seen)
+
+                pbar.set_postfix(tr_loss=tr_loss, val_loss=val_loss)
+
+                # deep copy the model
+                if phase == 'val' and epoch_loss < best_loss:
+                    best_loss = epoch_loss
+                    best_model_wts = copy.deepcopy(model.state_dict())
+                    
+            pbar.update(1)
+    
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Loss: {:4f}'.format(best_loss))
+    
+    
+    writer.flush()
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+
+if __name__=='__main__':
+
+    torch.cuda.empty_cache()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    print('found device: ', device)
+
+    # where the training results should go
+    results_dir = remove_and_create_dir(SCRATCH_DIR + '/DNN_train_taxinet/')
+
+    # where raw images and csvs are saved
+    BASE_DATALOADER_DIR = DATA_DIR + 'nominal_conditions_'
+
+    train_dir = BASE_DATALOADER_DIR + 'train/'
+    val_dir = BASE_DATALOADER_DIR + 'val/'
+
+    train_options = {"epochs": 50,
+                     "learning_rate": 1e-3, 
+                     "results_dir": results_dir,
+                     "train_dir": train_dir, 
+                     "val_dir": val_dir
+                     }
+
+    dataloader_params = {'batch_size': 512,
+                         'shuffle': True,
+                         'num_workers': 12,
+                         'drop_last': False}
+
+    # MODEL
+    # instantiate the model and freeze all but penultimate layers
+    model = TaxiNetDNN()
+    model = freeze_model(model)
+
+    # DATALOADERS
+    # instantiate the model and freeze all but penultimate layers
+    train_dataset = TaxiNetDataset(train_options['train_dir'])
+    val_dataset = TaxiNetDataset(train_options['val_dir'])
+
+    train_loader = DataLoader(train_dataset, **dataloader_params)
+    val_loader = DataLoader(val_dataset, **dataloader_params)
+
+
+    # OPTIMIZER
+    optimizer = torch.optim.Adam(model.parameters(),
+                                 lr=train_options["learning_rate"],
+                                 amsgrad=True)
+
+    # LOSS FUNCTION
+    loss_func = torch.nn.MSELoss().to(device)
+
+    # DATASET INFO
+    datasets = {}
+    datasets['train'] = train_dataset
+    datasets['val'] = val_dataset
+
+    dataloaders = {}
+    dataloaders['train'] = train_loader
+    dataloaders['val'] = val_loader
+
+    # train the DNN
+    train_model(model, datasets, dataloaders, loss_func, optimizer, device, num_epochs=train_options['epochs'], log_every=100)
--- src/train_DNN/split_train_val.py
+++ src/train_DNN/split_train_val.py
@@ -14,40 +14,42 @@ SCRATCH_DIR = NASA_ULI_ROOT_DIR + '/scratch/'
 data_dir = NASA_ULI_ROOT_DIR + '/data/'
 src_data_dir = data_dir + 'nominal_conditions/'
 
-filename_list = os.listdir(src_data_dir)
-png_files = [files for files in filename_list if files.endswith('.png')]
-episodes_list = list(set([x.split('_')[4] for x in png_files]))
+if __name__ == '__main__':
 
-num_train_episodes = 12
-train_start = 0
-train_end = num_train_episodes
-num_test_episodes = 4
-test_start = train_end
-test_end = train_end + num_test_episodes
-num_val_episodes = 4
-val_start = test_end
-val_end = test_end + num_val_episodes
+    filename_list = os.listdir(src_data_dir)
+    png_files = [files for files in filename_list if files.endswith('.png')]
+    episodes_list = list(set([x.split('_')[4] for x in png_files]))
 
-shuffled_episodes = np.random.permutation(episodes_list)
-train_episodes = shuffled_episodes[train_start:train_end]
-test_episodes = shuffled_episodes[test_start:test_end]
-val_episodes = shuffled_episodes[val_start:val_end]
+    num_train_episodes = 12
+    train_start = 0
+    train_end = num_train_episodes
+    num_test_episodes = 4
+    test_start = train_end
+    test_end = train_end + num_test_episodes
+    num_val_episodes = 4
+    val_start = test_end
+    val_end = test_end + num_val_episodes
 
-print("train_episodes: ", train_episodes)
-print("test_episodes: ", test_episodes)
-print("val_episodes: ", val_episodes)
+    shuffled_episodes = np.random.permutation(episodes_list)
+    train_episodes = shuffled_episodes[train_start:train_end]
+    test_episodes = shuffled_episodes[test_start:test_end]
+    val_episodes = shuffled_episodes[val_start:val_end]
 
-train_dir = remove_and_create_dir(data_dir + 'nominal_conditions_train/')
-test_dir = remove_and_create_dir(data_dir + 'nominal_conditions_test/')
-val_dir = remove_and_create_dir(data_dir + 'nominal_conditions_val/')
+    print("train_episodes: ", train_episodes)
+    print("test_episodes: ", test_episodes)
+    print("val_episodes: ", val_episodes)
 
-for image in png_files:
-    episode_num = image.split('_')[4]
-    if episode_num in train_episodes:
-        copyfile(src_data_dir + image, train_dir + image)
-    elif episode_num in test_episodes:
-        copyfile(src_data_dir + image, test_dir + image)
-    elif episode_num in val_episodes:
-        copyfile(src_data_dir + image, val_dir + image)
-    else:
-        print("Unhandled case.")
+    train_dir = remove_and_create_dir(data_dir + 'nominal_conditions_train/')
+    test_dir = remove_and_create_dir(data_dir + 'nominal_conditions_test/')
+    val_dir = remove_and_create_dir(data_dir + 'nominal_conditions_val/')
+
+    for image in png_files:
+        episode_num = image.split('_')[4]
+        if episode_num in train_episodes:
+            copyfile(src_data_dir + image, train_dir + image)
+        elif episode_num in test_episodes:
+            copyfile(src_data_dir + image, test_dir + image)
+        elif episode_num in val_episodes:
+            copyfile(src_data_dir + image, val_dir + image)
+        else:
+            print("Unhandled case.")
