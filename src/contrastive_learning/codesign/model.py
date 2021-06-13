8c19df1c2c788d1f35e03e8c6b7a99f47d65111d
csandeep90 <vss.emvr@gmail.com>
2021-06-12 20:47:08 -0700

    updates

--- src/contrastive_learning/codesign/model.py
+++ src/contrastive_learning/codesign/model.py
@@ -23,7 +23,21 @@ class DNN(nn.Module):
 
         # remember no relu on last layer!
         x = self.fc3(x)
-
         return x
 
+class TaskNet(nn.Module):
+    def __init__(self, model_name="tasknet"):
+        super(TaskNet, self).__init__()
+
+        self.DNN = DNN()
+        self.MPC = MPC()
+
+    def forward(self, x_vector, v_robot):
+        x_robot = x_vector[:,0]
+        x_target = x_vector[:, 0]
+
+        phat = self.DNN(x_vector)
+        u_mpc, x_mpc, J_mpc = self.MPC(x_robot, v_robot, phat)
+
+        return phat, u_mpc, x_mpc, J_mpc
 
